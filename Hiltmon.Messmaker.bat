27fcc47a589c9730540a1f0f03a5fb94ed7ba402
Jim Sferas <sferas@gmail.com>
2014-09-19 16:54:47 -0600

    Making yer mess early.
    All of this throw away.  But, it's pretty cute.

---
+++ Hiltmon.Messmaker.bat
@@ -0,0 +1,17 @@
+
+rem	Project sub-folder mess maker
+rem	http://hiltmon.com/blog/2012/06/30/project-folder-layout/
+rem
+
+mkdir archive
+mkdir artwork
+mkdir code
+mkdir data
+mkdir design
+mkdir documents
+mkdir management
+mkdir media
+mkdir notes
+mkdir partners
+mkdir support
+mkdir tools
